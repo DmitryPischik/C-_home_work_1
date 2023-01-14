390f8998c650c27251a2ba79106cdde13a576834
Dmitriy Pischik <pischik@bk.ru>
2023-01-15 09:53:41 +1000

    Added folder Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,6 @@
+﻿// Выяснить является ли число чётным
+
+Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+if (number % 2 == 0) Console.WriteLine("Число " + number + " чётное");
+else Console.WriteLine("Число " + number + " нечётное");
