bcba05ea7f7f562e9d68ca3404b419eb9d6377d9
Dmitriy Pischik <pischik@bk.ru>
2023-01-15 09:25:28 +1000

    Added folder Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("Введите первое целое положительное число: ");
+int number1 = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе целое положительное число: ");
+int number2 = int.Parse(Console.ReadLine() ?? "0");
+
+if (number2 * number2 == number1)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
