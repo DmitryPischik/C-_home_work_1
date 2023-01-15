edd8f4e1e0c14ec8efdef463518bb88428c699cf
Dmitriy Pischik <pischik@bk.ru>
2023-01-15 10:12:17 +1000

    Added folder Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать четные числа от 1 до N
+
+Console.Write("Введите целое число больше 1: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+int i = 1;
+while (i <= N)
+{
+    if (i % 2 == 0) 
+    {
+        Console.WriteLine(i);
+    }
+    i++;
+}
