36605975181342094891a456f7b1e66574bb21db
Dmitriy Pischik <pischik@bk.ru>
2023-01-15 10:43:13 +1000

    Added folder Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,9 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.Write("Введите целое трёхзначное число: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+int n1 = N / 100;
+int n2 = N % 10;
+
+Console.WriteLine(n1 * 10 + n2);
