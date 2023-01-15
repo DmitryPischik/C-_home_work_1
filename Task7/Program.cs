5ba0c4735620c49f5ccbf1d3f6b97efbc46730a3
Dmitriy Pischik <pischik@bk.ru>
2023-01-15 10:36:39 +1000

    Added folder Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,10 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int n = new Random().Next(10, 100);
+Console.WriteLine(n);
+
+int n1 = n / 10;
+int n2 = n % 10;
+int max = n1;
+if (n2 > n1) max = n2;
+Console.WriteLine("наибольшая цифра числа: " + max);
