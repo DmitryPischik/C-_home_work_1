e8679fea8442205546d7b822c0c31c04ed1b5272
Dmitriy Pischik <pischik@bk.ru>
2023-01-15 10:27:55 +1000

    Added folder Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.Write("Введите целое трёхзначное число: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine((N - ((N / 100) *100)) / 10);
