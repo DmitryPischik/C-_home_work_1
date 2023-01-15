b5a220212b9c799bd7ff1408ea15c65b4c94a3e7
Dmitriy Pischik <pischik@bk.ru>
2023-01-15 10:56:22 +1000

    Added folder Task9

---
+++ Task9/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Write("Задайте целое число: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+int random_number = new Random().Next(1, 1000);
+
+if (random_number % N == 0)
+{
+    Console.WriteLine("Число " + random_number + " кратно " + N);
+}
+else
+{
+    int lost = random_number % N;
+    Console.WriteLine("Число " + random_number + " некратно " + N + ". Остаток равен: " + lost);
+}
