6efaa548d8f06b1923d900c1ba49537671702a8f
Dmitriy Pischik <pischik@bk.ru>
2023-01-15 11:07:43 +1000

    Added folder Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+int random_number = new Random().Next(1, 1000);
+int div = random_number / 10;
+
+if (div < 10)
+{
+    Console.WriteLine("В числе " + random_number + " нет третьей цифры ");
+}
+else
+{
+    Console.WriteLine((random_number - div * 10) + " - третья цифра числа " + random_number);
+}
